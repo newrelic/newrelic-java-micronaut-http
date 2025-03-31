adac09278723fc97f1190aebb19030ef22c81d9f
Doug Hilpipre <dhilpipre@newrelic.com>
2025-03-31 09:47:55 -0500

    added additional Http transaction captures

--- micronaut-http-server-netty-2/src/main/java/io/micronaut/http/server/netty/RoutingInBoundHandler.java
+++ micronaut-http-server-netty-2/src/main/java/io/micronaut/http/server/netty/RoutingInBoundHandler.java
@@ -1,15 +1,15 @@
 package io.micronaut.http.server.netty;
 
-import java.util.List;
-import java.util.logging.Level;
+import java.net.URI;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.TransactionNamePriority;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.newrelic.instrumentation.micronaut.netty_2.Utils;
 
-import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpMethod;
 import io.micronaut.web.router.RouteMatch;
 import io.netty.channel.ChannelHandlerContext;
 
@@ -18,6 +18,23 @@ abstract class RoutingInBoundHandler {
 
 	@Trace
 	protected void channelRead0(ChannelHandlerContext ctx, io.micronaut.http.HttpRequest<?> request) {
+		if(request != null) {
+			StringBuffer sb = new StringBuffer();
+			HttpMethod method = request.getMethod();
+			if(method != null) {
+				sb.append(method.name());
+			} else {
+				sb.append("UnknownMethod");
+			}
+			sb.append(" - ");
+			URI uri = request.getUri();
+			if(uri != null) {
+				sb.append(uri.toASCIIString());
+			} else {
+				sb.append("UnknownURI");
+			}
+			NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.FRAMEWORK_LOW, false, "Micronaut-Netty", sb.toString());
+		}
 		Weaver.callOriginal();
 	}		
 		
@@ -26,13 +43,6 @@ abstract class RoutingInBoundHandler {
 	@SuppressWarnings("unused")
 	private void handleRouteMatch(RouteMatch<?> route, NettyHttpRequest<?> request,ChannelHandlerContext context, boolean skipOncePerRequest) {
 		Utils.decorateWithRoute(route);
-		HttpHeaders headers = request.getHeaders();
-		if(headers != null) {
-			for(String key : headers.names()) {
-				List<String> values = headers.getAll(key);
-				NewRelic.getAgent().getLogger().log(Level.FINE, "Header values for {0}: {1}", key, values);
-			}
-		}
 		Weaver.callOriginal();
 	}
 		
--- micronaut-http-server-netty-3/src/main/java/io/micronaut/http/server/netty/RoutingInBoundHandler.java
+++ micronaut-http-server-netty-3/src/main/java/io/micronaut/http/server/netty/RoutingInBoundHandler.java
@@ -1,15 +1,14 @@
 package io.micronaut.http.server.netty;
 
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
+import java.net.URI;
 
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.TransactionNamePriority;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 
-import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpMethod;
 import io.netty.channel.ChannelHandlerContext;
 
 @Weave
@@ -18,14 +17,21 @@ abstract class RoutingInBoundHandler {
 	@Trace(dispatcher = true)
 	protected void channelRead0(ChannelHandlerContext ctx, io.micronaut.http.HttpRequest<?> request) {
 		if(request != null) {
-			HttpHeaders headers = request.getHeaders();
-			if(headers != null) {
-				Map<String, List<String>> headermap = headers.asMap();
-				for(String key : headermap.keySet()) {
-					List<String> values = headermap.get(key);
-					NewRelic.getAgent().getLogger().log(Level.FINE, "\tHeader: {0} = {1}", key, values);
-				}
+			StringBuffer sb = new StringBuffer();
+			HttpMethod method = request.getMethod();
+			if(method != null) {
+				sb.append(method.name());
+			} else {
+				sb.append("UnknownMethod");
 			}
+			sb.append(" - ");
+			URI uri = request.getUri();
+			if(uri != null) {
+				sb.append(uri.toASCIIString());
+			} else {
+				sb.append("UnknownURI");
+			}
+			NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.FRAMEWORK_LOW, false, "Micronaut-Netty", sb.toString());
 		}
 		Weaver.callOriginal();
 	}		
